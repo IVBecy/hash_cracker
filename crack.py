9ebc1a653a7d48aa8387fa4b83715231b2625b43
IVBecy <57770911+IVBecy@users.noreply.github.com>
2020-06-13 01:10:11 +0100

    Add files via upload

--- crack.py
+++ crack.py
@@ -27,62 +27,211 @@ banner = """
 
 """
 
-# Usage
-def usage():
-    print("""
-    
-     Usage: py cracker.py [-pl] [-hl]
-    
-    -pl: The password list
-    -hl: The list of hashes
 
-    Example: py cracker.py -pl password.txt -hl hashes.txt
-    
-    """)
-    sys.exit()
+# Self advertising ;)
+print(banner)
+time.sleep(3)
 
-# Taking the terminal commands, then assigning them to variables
-
-if sys.argv[1] != "-pl":
-    usage()
-
-PASS_LIST = sys.argv[2]
-
-if sys.argv[3] != "-hl":
-    usage()
-
-HASH_LIST = sys.argv[4]
-
+#Taking inputs and assigning them
+print("\n")
+PASS_LIST = input("Enter the name of the password list:  ")
+print("\n")
+HASH_LIST = input("Enter the name od the hash file:  ")
+print("\n")
+HASH_METHOD = input(
+    "Enter your hashing algorithm (md5, sha1, sha256, sha512, sha3_256, sha3_512, blake2s, blake2b):  ")
+print("\n")
+CASE = input("Lower / Upper case hashes [l/u]:")
+print("\n")
 
 ######## Variables we need #######
 PASSWORDS = {}
 a = 0
 b = 1
 
-# Self advertising ;)
-print(banner)
-time.sleep(3)
-
 #######  Handling the passwords file  #########
-# And making a dictionary, with the passwords, to their assigned hash
-for get_pass_hash in range(file_length(PASS_LIST)):
-    passes = open(str(PASS_LIST), "r")
-    pass_list = passes.readlines()[a:b]
-    for p in pass_list:
-        p = str(p)
-        p = p.replace("[", "")
-        p = p.replace("]", "")
-        p = p.replace("'", "")
-        p = p.replace("\n", "")
-        p = p.replace(",", "")
-        passw = str(p).encode('utf-8')
-        hashed_passw = hashlib.sha1(passw).hexdigest()
-        PASSWORDS[hashed_passw] = p
-    a += 1
-    b += 1
-passes.close()
+# Reading the file, then hash them, to be able to convert later on
+
+###############################  SHA 1  ###################################
+
+if HASH_METHOD == "sha1":
+    for get_pass_hash in range(file_length(PASS_LIST)):
+        passes = open(str(PASS_LIST), "r")
+        pass_list = passes.readlines()[a:b]
+        for p in pass_list:
+            p = str(p)
+            p = p.replace("[", "")
+            p = p.replace("]", "")
+            p = p.replace("'", "")
+            p = p.replace("\n", "")
+            p = p.replace(",", "")
+            passw = str(p).encode('utf-8')
+            hashed_passw = hashlib.sha1(passw).hexdigest()
+            if CASE == "l":
+                PASSWORDS[hashed_passw] = p
+            elif CASE == "u":
+                PASSWORDS[hashed_passw.upper()] = p
+        a += 1
+        b += 1
+    passes.close()
+
+###############################  MD5  ###################################
+if HASH_METHOD == "md5":
+    for get_pass_hash in range(file_length(PASS_LIST)):
+        passes = open(str(PASS_LIST), "r")
+        pass_list = passes.readlines()[a:b]
+        for p in pass_list:
+            p = str(p)
+            p = p.replace("[", "")
+            p = p.replace("]", "")
+            p = p.replace("'", "")
+            p = p.replace("\n", "")
+            p = p.replace(",", "")
+            passw = str(p).encode('utf-8')
+            hashed_passw = hashlib.md5(passw).hexdigest()
+            if CASE == "l":
+                PASSWORDS[hashed_passw] = p
+            elif CASE == "u":
+                PASSWORDS[hashed_passw.upper()] = p
+        a += 1
+        b += 1
+    passes.close()
+
+###############################  SHA 256  ###################################
+if HASH_METHOD == "sha256":
+    for get_pass_hash in range(file_length(PASS_LIST)):
+        passes = open(str(PASS_LIST), "r")
+        pass_list = passes.readlines()[a:b]
+        for p in pass_list:
+            p = str(p)
+            p = p.replace("[", "")
+            p = p.replace("]", "")
+            p = p.replace("'", "")
+            p = p.replace("\n", "")
+            p = p.replace(",", "")
+            passw = str(p).encode('utf-8')
+            hashed_passw = hashlib.sha256(passw).hexdigest()
+            if CASE == "l":
+                PASSWORDS[hashed_passw] = p
+            elif CASE == "u":
+                PASSWORDS[hashed_passw.upper()] = p
+        a += 1
+        b += 1
+    passes.close()
+
+###############################  SHA 512  ###################################
+if HASH_METHOD == "sha512":
+    for get_pass_hash in range(file_length(PASS_LIST)):
+        passes = open(str(PASS_LIST), "r")
+        pass_list = passes.readlines()[a:b]
+        for p in pass_list:
+            p = str(p)
+            p = p.replace("[", "")
+            p = p.replace("]", "")
+            p = p.replace("'", "")
+            p = p.replace("\n", "")
+            p = p.replace(",", "")
+            passw = str(p).encode('utf-8')
+            hashed_passw = hashlib.sha512(passw).hexdigest()
+            if CASE == "l":
+                PASSWORDS[hashed_passw] = p
+            elif CASE == "u":
+                PASSWORDS[hashed_passw.upper()] = p
+        a += 1
+        b += 1
+    passes.close()
+
+###############################  SHA3 256  ###################################
+if HASH_METHOD == "sha3_256":
+    for get_pass_hash in range(file_length(PASS_LIST)):
+        passes = open(str(PASS_LIST), "r")
+        pass_list = passes.readlines()[a:b]
+        for p in pass_list:
+            p = str(p)
+            p = p.replace("[", "")
+            p = p.replace("]", "")
+            p = p.replace("'", "")
+            p = p.replace("\n", "")
+            p = p.replace(",", "")
+            passw = str(p).encode('utf-8')
+            hashed_passw = hashlib.sha3_256(passw).hexdigest()
+            if CASE == "l":
+                PASSWORDS[hashed_passw] = p
+            elif CASE == "u":
+                PASSWORDS[hashed_passw.upper()] = p
+        a += 1
+        b += 1
+    passes.close()
+
+###############################  SHA3 512  ###################################
+if HASH_METHOD == "sha3_512":
+    for get_pass_hash in range(file_length(PASS_LIST)):
+        passes = open(str(PASS_LIST), "r")
+        pass_list = passes.readlines()[a:b]
+        for p in pass_list:
+            p = str(p)
+            p = p.replace("[", "")
+            p = p.replace("]", "")
+            p = p.replace("'", "")
+            p = p.replace("\n", "")
+            p = p.replace(",", "")
+            passw = str(p).encode('utf-8')
+            hashed_passw = hashlib.sha3_512(passw).hexdigest()
+            if CASE == "l":
+                PASSWORDS[hashed_passw] = p
+            elif CASE == "u":
+                PASSWORDS[hashed_passw.upper()] = p
+        a += 1
+        b += 1
+    passes.close()
+
+###############################  BLAKE2s (256-bit)  ###################################
+if HASH_METHOD == "blake2s":
+    for get_pass_hash in range(file_length(PASS_LIST)):
+        passes = open(str(PASS_LIST), "r")
+        pass_list = passes.readlines()[a:b]
+        for p in pass_list:
+            p = str(p)
+            p = p.replace("[", "")
+            p = p.replace("]", "")
+            p = p.replace("'", "")
+            p = p.replace("\n", "")
+            p = p.replace(",", "")
+            passw = str(p).encode('utf-8')
+            hashed_passw = hashlib.blake2s(passw).hexdigest()
+            if CASE == "l":
+                PASSWORDS[hashed_passw] = p
+            elif CASE == "u":
+                PASSWORDS[hashed_passw.upper()] = p
+        a += 1
+        b += 1
+    passes.close()
+
+###############################  BLAKE2b (512-bit)  ###################################
+if HASH_METHOD == "blake2b":
+    for get_pass_hash in range(file_length(PASS_LIST)):
+        passes = open(str(PASS_LIST), "r")
+        pass_list = passes.readlines()[a:b]
+        for p in pass_list:
+            p = str(p)
+            p = p.replace("[", "")
+            p = p.replace("]", "")
+            p = p.replace("'", "")
+            p = p.replace("\n", "")
+            p = p.replace(",", "")
+            passw = str(p).encode('utf-8')
+            hashed_passw = hashlib.blake2b(passw).hexdigest()
+            if CASE == "l":
+                PASSWORDS[hashed_passw] = p
+            elif CASE == "u":
+                PASSWORDS[hashed_passw.upper()] = p
+        a += 1
+        b += 1
+    passes.close()
 
 ###########################################################################################
+
+############################  Finding hashes, in the dictionary  ##########################
 
 # Reseting values
 a = 0
@@ -99,10 +248,10 @@ for get_hashes in range(file_length(HASH_LIST)):
         p = p.replace("'", "")
         p = p.replace("\n", "")
         p = p.replace(",", "")
-        # Try to find the hash in the txt file, in the dictionary
+        # Try to find the hash in the txt file, and  in the dictionary
         try:
-            print("Password found for hash: ", p)
-            print("The password is:  ", PASSWORDS[p])
+            print("Password", PASSWORDS[p])
+            print("Hash for the password:", p)
             print("\n")
         except:
             pass
